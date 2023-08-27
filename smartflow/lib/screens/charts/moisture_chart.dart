42747052fa872c70b5daea192c8b49429fb81945
DevPrinceK <80714449+DevPrinceK@users.noreply.github.com>
2023-08-27 18:48:08 +0000

    Update moisture_chart.dart

--- smartflow/lib/screens/charts/moisture_chart.dart
+++ smartflow/lib/screens/charts/moisture_chart.dart
@@ -13,8 +13,8 @@ class MoistureChart extends StatefulWidget {
 }
 
 class _MoistureChartState extends State<MoistureChart> {
-  Map<dynamic, dynamic> last6Moistures = {};
   List<MoistureData> moistureData = [];
+  Map<dynamic, dynamic> data = {};
   // subscription
   late StreamSubscription<DatabaseEvent> moistureSubscription;
 
@@ -23,7 +23,7 @@ class _MoistureChartState extends State<MoistureChart> {
     moistureMap.forEach((key, value) {
       print("Key: $key, Value: $value");
       resultList.add(MoistureData(
-          location: key.toString(), moistureLevel: value.toDouble()));
+          timestamp: key.toString(), moistureLevel: value.toDouble()));
     });
     return resultList;
   }
@@ -32,15 +32,45 @@ class _MoistureChartState extends State<MoistureChart> {
   void initState() {
     super.initState();
 
-    // Get last 6 moistures
-    DatabaseReference moistRef =
-        FirebaseDatabase.instance.ref().child('bulkMoisture');
-    moistureSubscription = moistRef.limitToFirst(6).onValue.listen((event) {
+    DatabaseReference dataRef = FirebaseDatabase.instance.ref().child('data');
+    moistureSubscription = dataRef.limitToLast(6).onValue.listen((event) {
       setState(() {
-        last6Moistures = event.snapshot.value as Map<dynamic, dynamic>;
-        moistureData = convertMapToList(last6Moistures);
+        data = event.snapshot.value as Map<dynamic, dynamic>;
       });
-      print("Bulk Moistures: ${event.snapshot.value as Map<dynamic, dynamic>}");
+
+      Iterable<dynamic> values = data.values;
+      List<dynamic> lstData = values.toList();
+      List<int> moist = lstData.map<int>((i) => i['moisture']).toList();
+      List<double> timestampList =
+          lstData.map<double>((i) => i['timestamp']).toList();
+
+      // time string
+      List<String> timeStrings = timestampList.map((timestamp) {
+        DateTime dateTime =
+            DateTime.fromMillisecondsSinceEpoch((timestamp * 1000).toInt());
+        String timeString =
+            "${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}";
+        return timeString;
+      }).toList();
+
+      // set new values
+      setState(() {
+        moistureData = [
+          MoistureData(
+              timestamp: timeStrings[0], moistureLevel: moist[0].toDouble()),
+          MoistureData(
+              timestamp: timeStrings[1], moistureLevel: moist[1].toDouble()),
+          MoistureData(
+              timestamp: timeStrings[2], moistureLevel: moist[2].toDouble()),
+          MoistureData(
+              timestamp: timeStrings[3], moistureLevel: moist[3].toDouble()),
+          MoistureData(
+              timestamp: timeStrings[4], moistureLevel: moist[4].toDouble()),
+          MoistureData(
+              timestamp: timeStrings[5], moistureLevel: moist[5].toDouble()),
+        ];
+      });
+      print("Moisture Data: $moistureData");
     });
   }
 
@@ -61,7 +91,7 @@ class _MoistureChartState extends State<MoistureChart> {
       series: <ChartSeries>[
         ColumnSeries<MoistureData, String>(
           dataSource: moistureData,
-          xValueMapper: (MoistureData data, _) => data.location,
+          xValueMapper: (MoistureData data, _) => data.timestamp,
           yValueMapper: (MoistureData data, _) => data.moistureLevel,
           color: Colors.green, // Custom bar color
           dataLabelSettings: const DataLabelSettings(
@@ -78,8 +108,8 @@ class _MoistureChartState extends State<MoistureChart> {
 }
 
 class MoistureData {
-  final String location;
+  final String timestamp;
   final double moistureLevel;
 
-  MoistureData({required this.location, required this.moistureLevel});
+  MoistureData({required this.timestamp, required this.moistureLevel});
 }
